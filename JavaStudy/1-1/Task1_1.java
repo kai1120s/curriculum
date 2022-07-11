420b77ea02e9646d8c4d13aaf402765187710b2f
kai1120s <kai20s.cats@gmail.com>
2022-07-12 02:18:56 +0900

    Create Task1_1.java
    データ型の課題

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,26 @@
+public class Task1_1 {
+
+    public static void main(String[]args) {
+
+        int hogeInt = 50;
+
+        string hogeString = "テスト";
+
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [浮動小数点の表示]
+
+        double hogeDouble = 0.2;
+
+        int hogesInt = 100;
+
+        hogeString = "合格";
+
+        System.out.println("int型 = 合格 = " + hogeInt); 
+
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [hogeStringの数値を出力]
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+    }
+}
